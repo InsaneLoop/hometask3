dcc5c4cfaa265217b146073247c4bea47e09103c
InsaneLoop <121028213+InsaneLoop@users.noreply.github.com>
2024-11-21 18:56:01 +0200

    Update instance_run.sh

--- instance_run.sh
+++ instance_run.sh
@@ -4,4 +4,5 @@ aws ec2 run-instances --image-id ami-012967cc5a8c9f891 \
 --key-name hometask2 \
 --security-group-ids sg-0d97c6f05dd2de4c7 \
 --subnet-id  subnet-01d6a4e65da866e80 \
+--iam-instance-profile Name=SSMrole \
 --user-data file://script.sh
